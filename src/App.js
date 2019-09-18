640b9375503338e297bc9411c1a122cf6130395c
victor <bearbur@gmail.com>
2019-09-19 02:11:31 +0300

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import NavigationContainer from './containers/NavigationContainer';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <NavigationContainer/>
+      </header>
+      <main>
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
